dafb6c7bb04deabab75e339726b67daf4bf25cfe
ArenBoghozian8 <aren.mboghozian@gmail.com>
2018-03-01 18:58:29 -0800

    quaternion_to_euler_angle now returns radians rather than degrees

--- src/detection_calculation/src/detection_calculation_node/detection_calculation.py
+++ src/detection_calculation/src/detection_calculation_node/detection_calculation.py
@@ -14,14 +14,16 @@ global MyHumans
 global init_robot_pose
 global robot_pos_x, robot_pos_z, robot_pos_th
 
+#RosLaunch Parameters
 mission_number_ = rospy.get_param('~mission_number')
 robot_number_ = rospy.get_param('~robot_number#')
 
+#Config file dictinary
 MyHumans = yaml.load(open('human.yaml'))
 init_robot_pose = yaml.load(open('robot.yaml'))
 
 robot_pos_x = init_robot_pose[str(mission_number_)][str(robot_number_)]['x']
-robot_pos_z = init_robot_pose[str(mission_number_)][str(robot_number_)]['y'] #Must be changed to y after config fix
+robot_pos_z = init_robot_pose[str(mission_number_)][str(robot_number_)]['z'] x
 robot_pos_th = init_robot_pose[str(mission_number_)][str(robot_number_)]['theta']
 
 def process():
@@ -59,16 +61,16 @@ def quaternion_to_euler_angle(w, x, y, z):
 
   t0 = +2.0 * (w * x + y * z)
   t1 = +1.0 - 2.0 * (x * x + ysqr)
-  X = math.degrees(math.atan2(t0, t1))
+  X = math.atan2(t0, t1)
 
   t2 = +2.0 * (w * y - z * x)
   t2 = +1.0 if t2 > +1.0 else t2
   t2 = -1.0 if t2 < -1.0 else t2
-  Y = math.degrees(math.asin(t2))
+  Y = math.asin(t2)
 
   t3 = +2.0 * (w * z + x * y)
   t4 = +1.0 - 2.0 * (ysqr + z * z)
-  Z = math.degrees(math.atan2(t3, t4))
+  Z = math.atan2(t3, t4)
 	
   return X, Y, Z  
 
