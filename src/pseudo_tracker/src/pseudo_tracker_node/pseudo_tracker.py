f4dee1979e68925de1c72870b76ac034ed1bf618
ArenBoghozian8 <Aren.mboghozian@gmail.com>
2018-03-02 16:57:08 -0800

    Some changes

--- src/pseudo_tracker/src/pseudo_tracker_node/pseudo_tracker.py
+++ src/pseudo_tracker/src/pseudo_tracker_node/pseudo_tracker.py
@@ -5,15 +5,15 @@ from rospy import ROSException
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Pose
-from new_detection_messages.msg import CompiledMessage
-from new_detection_messages.msg import Human
+from new_detection_msgs.msg import CompiledMessage
+from new_detection_msgs.msg import Human
 import math
 
-
+global human_
 
 
 def main():
-	process()
+	print("Hello")
 
 
 if __name__ == "__main__":
